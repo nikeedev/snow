140ce6302ac4a5029468c679d810599efd8c19c5
nikeedev <nikita.gon.dev@gmail.com>
2024-01-01 16:55:12 +0100

    up

---
+++ snow/src/mesh/Rectangle.h
@@ -0,0 +1,24 @@
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+
+#include "glm/vec4.hpp"
+
+class Rectangle {
+public:
+    Rectangle(glm::vec4 color);
+private:
+    float vertices[] = {
+            // first triangle
+            0.5f,  0.5f, 0.0f,  // top right
+            0.5f, -0.5f, 0.0f,  // bottom right
+            -0.5f,  0.5f, 0.0f,  // top left
+            // second triangle
+            0.5f, -0.5f, 0.0f,  // bottom right
+            -0.5f, -0.5f, 0.0f,  // bottom left
+            -0.5f,  0.5f, 0.0f   // top left
+    };
+};
+
+
+
+#endif //RECTANGLE_H
